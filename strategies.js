038ceba2e62b307b1e015496c20011c0bddfa022
natasha-labs <zishazava@gmail.com>
2026-07-20 17:41:09 +0200

    Delete strategies.js

--- strategies.js
+++
@@ -1,7 +0,0 @@
-window.TEST_STRATEGIES = {
-  rescuer: { order: 1, title: "Спасатель", image: "assets/images/rescuer.svg" },
-  controller: { order: 2, title: "Контролер", image: "assets/images/controller.svg" },
-  achiever: { order: 3, title: "Достигатор", image: "assets/images/achiever.svg" },
-  avoidant: { order: 4, title: "Избегающий", image: "assets/images/avoidant.svg" },
-  dependent: { order: 5, title: "Зависимый", image: "assets/images/dependent.svg" }
-};
