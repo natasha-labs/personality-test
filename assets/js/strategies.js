1d77dc091df7600d544d079626bc365b853fcbed
natasha-labs <zishazava@gmail.com>
2026-07-20 18:53:53 +0200

    Delete assets/js directory

--- assets/js/strategies.js
+++
@@ -1,7 +0,0 @@
-window.TEST_STRATEGIES = {
-  rescuer: { order: 1, title: "Спасатель", image: "assets/images/rescuer.png" },
-  controller: { order: 2, title: "Контролер", image: "assets/images/controller.png" },
-  achiever: { order: 3, title: "Достигатор", image: "assets/images/achiever.png" },
-  avoidant: { order: 4, title: "Избегающий", image: "assets/images/avoidant.png" },
-  dependent: { order: 5, title: "Зависимый", image: "assets/images/dependent.png" }
-};
