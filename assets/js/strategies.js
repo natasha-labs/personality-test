2df28cd762e754eab648a0bc44e8509d7d3d3586
natasha-labs <zishazava@gmail.com>
2026-07-20 18:02:39 +0200

    Add files via upload

--- assets/js/strategies.js
+++ assets/js/strategies.js
@@ -1,7 +1,7 @@
 window.TEST_STRATEGIES = {
-  rescuer: { order: 1, title: "Спасатель", image: "assets/images/rescuer.svg" },
-  controller: { order: 2, title: "Контролер", image: "assets/images/controller.svg" },
-  achiever: { order: 3, title: "Достигатор", image: "assets/images/achiever.svg" },
-  avoidant: { order: 4, title: "Избегающий", image: "assets/images/avoidant.svg" },
-  dependent: { order: 5, title: "Зависимый", image: "assets/images/dependent.svg" }
+  rescuer: { order: 1, title: "Спасатель", image: "assets/images/rescuer.png" },
+  controller: { order: 2, title: "Контролер", image: "assets/images/controller.png" },
+  achiever: { order: 3, title: "Достигатор", image: "assets/images/achiever.png" },
+  avoidant: { order: 4, title: "Избегающий", image: "assets/images/avoidant.png" },
+  dependent: { order: 5, title: "Зависимый", image: "assets/images/dependent.png" }
 };
